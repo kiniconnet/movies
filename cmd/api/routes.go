c6a8278dcce8133680632514161f3e732da97c39
kiniconnet <kiniconnet@gmail.com>
2025-04-01 20:07:34 +0100

    thanks for updating

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,32 +1,20 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
-	"github.com/joho/godotenv"
 )
 
 func (app *application) routes() http.Handler {
-	// Load environment variable
-	if os.Getenv("ENV") != "production" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
 
 	// define routes
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
 
-	if os.Getenv("ENV") != "production" {
-		mux.Use(app.enableCORS)
-	}
+	mux.Use(app.enableCORS)
 
 	mux.Get("/", app.Home)
 	mux.Get("/api/movies", app.AllMovies)
